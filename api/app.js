902e632813396fbc90867fb5cee347b75440a2c1
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-10-10 23:16:18 +0500

    update

--- api/app.js
+++ api/app.js
@@ -5,15 +5,16 @@ var app = express();
 // Use express.json() middleware to parse JSON request bodies
 var cors = require('cors')
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
   app.options('/api/(.*)', cors()); // enable pre-flight across-the-board
 
-  app.use(cors({
-      origin: '*', // or '*' for testing
+  app.use(cors({ 
+       origin:true, // or '*' for testing
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Content-Type', 'Authorization'],
       credentials: true
