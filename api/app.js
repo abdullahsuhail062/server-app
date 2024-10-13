44681a43c111ecf10df14ce9dce752d0d7eb7b7d
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-10-14 01:33:39 +0500

    update

--- api/app.js
+++ api/app.js
@@ -33,13 +33,13 @@ app.post('/api/registerUser', function (req, res, next) {
     sameSite: 'None',  // Allows cross-site requests
     secure: true
   });
+  const {username, email, password}=req.body; // Destructure all variables one by one
   const errors = {};
 
   if (Object.keys(req.body).length === 0) {
      res.status(400).json({ errors: 'Request body is empty' });
   }else{res.status(200).json({message: 'Request body is not empty'})}
 
-  const {username, email, password}=req.body; // Destructure all variables one by one
  
   
 
