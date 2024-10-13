53f06653eaa8edc8b58423ab1a22b39d973697c8
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-10-14 03:59:06 +0500

    update

--- api/app.js
+++ api/app.js
@@ -72,7 +72,7 @@ app.post('/api/registerUser', function (req, res, next) {
  
 });
 app.get('/', function( req, res, next){
-  res.send('empty request found')
+  res.send(requestBody)
   console.log('empty request body');
   
 })
