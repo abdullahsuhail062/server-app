cb48a667230f90117ea2170683ff33156c09edee
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-10-14 19:02:29 +0500

    update

--- api/app.js
+++ api/app.js
@@ -43,7 +43,7 @@ app.post('/api/registerUser', function (req, res, next) {
   });
 
   if (req.body ) {
-    const strgfyObj = JSON.stringify(username)
+    const strgfyObj = JSON.stringify(req.body)
     res.status(200).json({message: strgfyObj})}
 
   // Validate username
