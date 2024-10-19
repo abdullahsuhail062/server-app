4d77a4f885806fbd99cbf9d6947c153fc32a196d
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-10-19 15:44:06 +0500

    update

--- api/app.js
+++ api/app.js
@@ -219,7 +219,7 @@ app.post('/api/registerUser', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT ||3000 // Default for local testing
+const PORT = 3000 // Default for local testing
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
