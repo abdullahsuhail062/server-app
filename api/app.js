243161e6e5f01bb99c35676b179d7a742f346187
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-09 14:55:29 +0500

    update

--- api/app.js
+++ api/app.js
@@ -288,6 +288,30 @@ app.post('/api/loginUser', async (req, res) => {
   }
 });
 
+app.get('/api/fetchUserProfile', async (res, req)=> {
+  const userId = req.user.id; // Retrieve user ID from the request object or token
+
+  try {
+    const result = await client.query(
+      'SELECT username, email FROM users WHERE id = $1',
+      [userId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const user = result.rows[0];
+    res.json(user);
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
