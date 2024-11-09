05cc22b4731ad088ecb8133f81930a85ca15cddb
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-09 07:39:24 +0500

    update

--- api/app.js
+++ api/app.js
@@ -169,6 +169,7 @@ const client = new Client({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
+  
   ssl: {
     rejectUnauthorized: false
   },
