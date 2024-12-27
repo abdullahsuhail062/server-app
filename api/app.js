c313798c0889cf9d0c2ad8a57487b7cd8c457ecd
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-12-27 08:11:15 +0500

    update

--- api/app.js
+++ api/app.js
@@ -271,7 +271,7 @@ app.post('/api/tasks', async (req, res) => {
 
       );
       
-      res.status(201).json(result.rows[0], result.rows[0].id);
+      res.status(201).json(result.rows[0]);
   } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
