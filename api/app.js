cf335393ffce35b9d01072033df8b2d09ec13423
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-12-29 09:43:56 +0500

    update

--- api/app.js
+++ api/app.js
@@ -302,8 +302,8 @@ app.put('/api/updateTask/:taskId', async (req, res) => {
 
 
 // Endpoint to delete a task by id
-app.delete('/api/deleteTask/:taskId', async (req, res) => {
-  const { taskId } = req.params; // Corrected to match the route parameter name
+app.delete('/api/deleteTask', async (req, res) => {
+  const { taskId } = req.body; // Corrected to match the route parameter name
 
   try {
     const result = await client.query('DELETE FROM tasks WHERE id = $1 RETURNING *', [taskId]);
