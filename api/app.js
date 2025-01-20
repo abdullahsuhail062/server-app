96d8fcbcc29e32d5913cb50f4ad92df8e970c7a5
Your Name <you@example.com>
2025-01-20 08:36:30 +0500

    update

--- api/app.js
+++ api/app.js
@@ -41,7 +41,7 @@ const client = new Client({
 });
 
 client.connect()
-  .then(() => console.log('Connected to the database!'))
+  .then(() => console.log('Connected to the database!!!'))
   .catch(err => console.error('Connection error', err.stack));
 
 // Register User Route
